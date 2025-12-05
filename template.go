3da69973e69da55f6172754ffc742303b7aacd51
Gurleen Sethi <sarusethi@rocketmail.com>
2025-12-04 23:16:56 -0500

    read user input

--- template.go
+++ template.go
@@ -4,8 +4,12 @@ import (
 	"bytes"
 	"embed"
 	"errors"
+	"fmt"
 	"io/fs"
+	"os"
+	"strconv"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
 	"gopkg.in/yaml.v3"
 )
@@ -15,11 +19,59 @@ var templateFS embed.FS
 
 var ErrNotExists = errors.New("template does not exist")
 
+type FloatValidationRule struct {
+	Message string   `yaml:"message"`
+	Min     *float64 `yaml:"min,omitempty"`
+	Max     *float64 `yaml:"max,omitempty"`
+}
+
+type StringValidationRule struct {
+	Pattern string `yaml:"pattern"`
+	Message string `yaml:"message"`
+	MinLen  *int   `yaml:"min_len,omitempty"`
+	MaxLen  *int   `yaml:"max_len,omitempty"`
+}
+
+type TemplateInputValidation struct {
+	Float  *FloatValidationRule  `yaml:"float,omitempty"`
+	String *StringValidationRule `yaml:"string,omitempty"`
+}
+
+func (tiv *TemplateInputValidation) Validate(input string) string {
+	if tiv.Float != nil {
+		numValue, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			if tiv.Float.Message != "" {
+				return tiv.Float.Message
+			}
+
+			return "invalid number"
+		}
+
+		if tiv.Float.Min != nil && float64(numValue) < *tiv.Float.Min {
+			return fmt.Sprintf("number must be greater than %v", *tiv.Float.Min)
+		}
+
+		if tiv.Float.Max != nil && float64(numValue) > *tiv.Float.Max {
+			return fmt.Sprintf("number must be less than %v", *tiv.Float.Max)
+		}
+	}
+
+	if tiv.String != nil {
+		// Validate as string
+		// (Implementation of string validation can be added here)
+	}
+
+	return ""
+}
+
 type TemplateInput struct {
-	Name     string `yaml:"name"`
-	Default  string `yaml:"default"`
-	Required bool   `yaml:"required"`
-	Help     string `yaml:"help"`
+	Name     string                  `yaml:"name"`
+	Default  string                  `yaml:"default"`
+	Required bool                    `yaml:"required"`
+	Help     string                  `yaml:"help"`
+	Pattern  string                  `yaml:"pattern"`
+	Validate TemplateInputValidation `yaml:"validate,omitempty"`
 }
 
 type TemplateFile string
@@ -61,6 +113,10 @@ func ReadUserInputs(td *TemplateData) (map[string]string, error) {
 		userInputs[input.Name] = input.Default
 	}
 
+	if len(td.Inputs) == 0 {
+		return userInputs, nil
+	}
+
 	var fields []huh.Field
 	for _, input := range td.Inputs {
 		field := huh.NewInput().
@@ -68,8 +124,16 @@ func ReadUserInputs(td *TemplateData) (map[string]string, error) {
 			Description(input.Help).
 			Validate(func(s string) error {
 				if input.Required && s == "" {
-					return errors.New("this field is required")
+					return errors.New(input.Name + " is required")
 				}
+
+				validationMessage := input.Validate.Validate(s)
+				if validationMessage != "" {
+					return errors.New(input.Name + " " + validationMessage)
+				}
+
+				userInputs[input.Name] = s
+
 				return nil
 			}).
 			Key(input.Name)
@@ -79,7 +143,7 @@ func ReadUserInputs(td *TemplateData) (map[string]string, error) {
 
 	form := huh.NewForm(
 		huh.NewGroup(fields...),
-	)
+	).WithProgramOptions(tea.WithOutput(os.Stdout))
 
 	err := form.Run()
 	if err != nil {
